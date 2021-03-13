20b731120030109677667d93eda4042d02cb418f
JSischo <jsischo@uwalumni.com>
2021-03-12 22:17:25 -0600

    Project completion uploads.

--- static/js/app.js
+++ static/js/app.js
@@ -26,7 +26,7 @@ function buildTable(data) {
 
 function handleClick(){
     // Grab the datetime value from the filter
-    let date = d3.select(#datetime).property("value");
+    let date = d3.select("#datetime").property("value");
     let filteredData = tableData;
     
     // Check to see if a date was entered and filter the 
